836a2c67ccbe7666fb8d9bc2003f89612eb31382
ducanh005 <vuducanh039@gmail.com>
2025-07-11 09:21:13 +0700

    UI admin page

---
+++ src/components/AdminProduct/AdminProduct.jsx
@@ -0,0 +1,19 @@
+import { Button } from "antd";
+import { WrapperHeader } from "./style";
+import {  PlusOutlined } from '@ant-design/icons';
+import TableComponent from "../TableComponent/TableComponent";
+const AdminProduct = () => {
+    return (
+        <div>
+            <WrapperHeader>Quản lý sản phẩm</WrapperHeader>
+            <div style={{marginTop:'10px'}}>
+                <Button style={{height:'150px', width:'150px', borderRadius:'6px', borderStyle:'dashed'}}><PlusOutlined style={{fontSize:'60px'}}/></Button>
+            </div>
+            <div style={{marginTop:'20px'}}> 
+                <TableComponent/>
+            </div>
+        </div>
+    );
+}
+
+export default AdminProduct;
--- src/pages/AdminPage/AdminPage.jsx
+++ src/pages/AdminPage/AdminPage.jsx
@@ -3,33 +3,26 @@ import { useState } from "react";
 import { getItem } from "../../utils";
 import { AppstoreOutlined, UserOutlined } from "@ant-design/icons";
 import HeaderComponent from "../../components/HeaderComponent/HeaderComponent";
+import AdminProduct from "../../components/AdminProduct/AdminProduct";
+import AdminUser from "../../components/AdminUser/AdminUser";
 const AdminPage = () => {
     const items = [
-        getItem('Người dùng', 'sub1', <UserOutlined/>, [
-            getItem('Option 1', '1'),
-            getItem('Option 2', '2'),
-            getItem('Option 3', '3'),
-            getItem('Option 4', '4'),
-        ]),
-        getItem('Sản phẩm', 'sub2', <AppstoreOutlined/>, [
-            getItem('Option 5', '5'),
-            getItem('Option 6', '6'),
-            getItem('Option 7', '7'),
-            getItem('Option 8', '8'),
-        ]),
+        getItem('Người dùng', 'user', <UserOutlined/>),
+        getItem('Sản phẩm', 'product', <AppstoreOutlined/>),
         
     ]
-    const rootSubmenuKeys = ['user', 'product'];
-    const [openKeys, setOpenKeys] = useState(['user']);
     const [keySelected, setKeySelected] = useState('');
-    const onOpenChange = (keys) => {
-        const latestOpenKey = keys.find(key => openKeys.indexOf(key) === -1);
-        if (rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
-            setOpenKeys(keys);
-        } else {
-            setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
+    const renderPage =(key) => {
+        switch (key) {
+            case 'user':
+                return <AdminUser/>
+            case 'product':
+                return <AdminProduct/>
+            default:
+                return <></>
         }
     }
+    
     const handleOnClick = ({key}) => {
         setKeySelected(key);
     }
@@ -39,15 +32,15 @@ const AdminPage = () => {
             <div style={{display:'flex'}}>
                 <Menu
                     mode="inline"
-                    openKeys={openKeys}
-                    onOpenChange={onOpenChange}
-                    style={{ width: 256 }}
+                    style={{ width: 256,
+                        boxShadow: '1px 1px 2px #ccc',
+                        height: '100vh',
+                     }}
                     items={items}
                     onClick={handleOnClick}
                 />
-                <div style={{flex:1}}>
-                    {keySelected === '6' && <span>key la 6</span>}
-                    <span>1</span>
+                <div style={{flex:1, padding:'15px'}}>
+                   {renderPage(keySelected)}
                 </div>
             </div>
        </>
